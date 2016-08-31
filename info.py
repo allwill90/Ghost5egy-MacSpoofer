f630a46e6eb5370c6dec501cf4b704c2d3f1d1da
ghost5egy <28121984>
2016-08-31 07:44:39 +0200

    test 6
    trying to fix admin request on windows

--- info.py
+++ info.py
@@ -3,15 +3,28 @@ import subprocess
 import sys
 import os
 import os.path
+import ctypes
 
-def reqadminwin():
-	import win32com.shell.shell as shell
-	ASADMIN = 'asadmin'
+def reqadminwin(argv=None, debug=False):
+    shell32 = ctypes.windll.shell32
+    if argv is None and shell32.IsUserAnAdmin():
+        return True
 
-	if sys.argv[-1] != ASADMIN:
-		script = os.path.abspath(sys.argv[0])
-		params = ' '.join([script] + sys.argv[1:] + [ASADMIN])
-		shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
+    if argv is None:
+        argv = sys.argv
+    if hasattr(sys, '_MEIPASS'):
+        # Support pyinstaller wrapped program.
+        arguments = map(unicode, argv[1:])
+    else:
+        arguments = map(unicode, argv)
+    argument_line = u' '.join(arguments)
+    executable = unicode(sys.executable)
+    if debug:
+        print 'Command line: ', executable, argument_line
+    ret = shell32.ShellExecuteW(None, u"runas", executable, argument_line, None, 1)
+    if int(ret) <= 32:
+        return False
+    return None
 
 def checkcon():
 	if sys.platform == 'win32':
